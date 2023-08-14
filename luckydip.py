e0e50a1ef20f2982f559db220ac542a881942fec
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2023-08-14 23:00:04 +0100

    Update luckydip.py

--- luckydip.py
+++ luckydip.py
@@ -1,43 +1,86 @@
 import random
 import pandas as pd
 from collections import Counter
+from typing import List, Tuple
+import logging
 
-# Function to check if the combination is valid
-def is_valid_combination(numbers):
+# Configuration dictionary for constraints
+CONFIG = {
+    "main_numbers_count": 5,
+    "lucky_stars_count": 2,
+    "main_numbers_range": (1, 50),
+    "lucky_stars_range": (1, 12),
+    "even_count_range": (2, 3),
+    "odd_count_range": (2, 3),
+    "low_count_range": (2, 3),
+    "high_count_range": (2, 3),
+}
+
+def is_valid_combination(numbers: List[int], number_range: Tuple[int, int]) -> bool:
+    """Check if the combination of numbers is valid."""
+    if not all(number_range[0] <= num <= number_range[1] for num in numbers):
+        return False
+    if any(numbers[i] + 1 == numbers[i + 1] for i in range(len(numbers) - 1)):
+        return False
     even_count = sum(1 for num in numbers if num % 2 == 0)
     odd_count = len(numbers) - even_count
-    if even_count < 2 or even_count > 3 or odd_count < 2 or odd_count > 3:
+    if not CONFIG["even_count_range"][0] <= even_count <= CONFIG["even_count_range"][1]:
         return False
-    low_count = sum(1 for num in numbers if num <= 25)
-    if low_count < 2 or low_count > 3:
+    if not CONFIG["odd_count_range"][0] <= odd_count <= CONFIG["odd_count_range"][1]:
+        return False
+    low_count = sum(1 for num in numbers if num <= number_range[1] // 2)
+    if not CONFIG["low_count_range"][0] <= low_count <= CONFIG["low_count_range"][1]:
         return False
     return True
 
-# Function to generate ticket considering historical data
-def generate_ticket(main_freq, lucky_star_freq):
-    main_numbers_pool = [num for num, freq in main_freq.items() for _ in range(freq)]
-    lucky_star_pool = [num for num, freq in lucky_star_freq.items() for _ in range(freq)]
-    main_numbers = []
-    while not main_numbers or not is_valid_combination(main_numbers):
-        main_numbers = sorted(random.choices(main_numbers_pool, k=5))
-    lucky_stars = sorted(random.choices(lucky_star_pool, k=2))
-    return main_numbers, lucky_stars
+def generate_ticket(freq: Counter, k: int = 5) -> List[int]:
+    """Generate a ticket based on the frequency of numbers."""
+    numbers, weights = zip(*freq.items())
+    weights = [weight / sum(weights) for weight in weights]
+    numbers = sorted(random.choices(numbers, k=k, weights=weights))
+    while not is_valid_combination(numbers, CONFIG["main_numbers_range"] if k == CONFIG["main_numbers_count"] else CONFIG["lucky_stars_range"]):
+        numbers = sorted(random.choices(numbers, k=k, weights=weights))
+    return numbers
 
-# Load the historical EuroMillions draw data
-url = "https://raw.githubusercontent.com/userVatsal/euromillions-lucky-dip-generator/main/euromillions-draw-history.csv"
-data = pd.read_csv(url)
+def generate_numbers(data: pd.DataFrame, columns: List[str], k: int = 5) -> List[int]:
+    """Generate numbers based on historical data and given columns."""
+    frequency = data[columns].stack().value_counts()
+    return generate_ticket(frequency, k)
 
-# Analyze the frequency of main numbers and lucky stars
-main_numbers_frequency = Counter()
-lucky_star_frequency = Counter()
-for i in range(1, 6):
-    main_numbers_frequency.update(data[f'Ball {i}'].clip(lower=1, upper=50))
-for i in range(1, 3):
-    lucky_star_frequency.update(data[f'Lucky Star {i}'].clip(lower=1, upper=12))
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Clean data by clipping values within valid ranges."""
+    for i in range(1, CONFIG["main_numbers_count"] + 1):
+        data[f'Ball {i}'] = data[f'Ball {i}'].clip(lower=CONFIG["main_numbers_range"][0], upper=CONFIG["main_numbers_range"][1])
+    for i in range(1, CONFIG["lucky_stars_count"] + 1):
+        data[f'Lucky Star {i}'] = data[f'Lucky Star {i}'].clip(lower=CONFIG["lucky_stars_range"][0], upper=CONFIG["lucky_stars_range"][1])
+    return data
 
-# Generate the lucky dip ticket considering historical data
-main_numbers, lucky_stars = generate_ticket(main_numbers_frequency, lucky_star_frequency)
+def main():
+    logging.basicConfig(level=logging.INFO)
+    try:
+        logging.info("Starting the EuroMillions Lucky Dip Generator...")
+        url = "https://raw.githubusercontent.com/userVatsal/euromillions-lucky-dip-generator/main/euromillions-draw-history.csv"
+        data = pd.read_csv(url)
+        data = clean_data(data)
 
-print("Main Numbers:", main_numbers)
-print("Lucky Stars:", lucky_stars)
+        main_numbers_columns = [f'Ball {i}' for i in range(1, CONFIG["main_numbers_count"] + 1)]
+        lucky_star_columns = [f'Lucky Star {i}' for i in range(1, CONFIG["lucky_stars_count"] + 1)]
+
+        main_numbers = generate_numbers(data, main_numbers_columns)
+        lucky_stars = generate_numbers(data, lucky_star_columns, k=CONFIG["lucky_stars_count"])
+
+        print("Main Numbers:", main_numbers)
+        print("Lucky Stars:", lucky_stars)
+
+        logging.info("Exiting the EuroMillions Lucky Dip Generator.")
+    except pd.errors.EmptyDataError:
+        logging.error("The data file is empty.")
+    except pd.errors.ParserError:
+        logging.error("There was an error parsing the data file.")
+    except Exception as e:
+        logging.error("An unexpected error occurred: %s", str(e))
+
+if __name__ == "__main__":
+    main()
+
 
