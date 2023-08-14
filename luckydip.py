1953218a85cd8201d68baa7a768e22b29e65cdb4
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2023-08-14 21:58:31 +0100

    Update luckydip.py

--- luckydip.py
+++ luckydip.py
@@ -1,26 +1,20 @@
 import random
-import pandas as pd
 from collections import Counter
+import pandas as pd
+from typing import List, Tuple
 
 # Function to check if the combination is valid
-def is_valid_combination(numbers):
-    even_count = sum(1 for num in numbers if num % 2 == 0)
-    if even_count < 2 or even_count > 3:
-        return False
-    low_count = sum(1 for num in numbers if num <= 25)
-    if low_count < 2 or low_count > 3:
-        return False
-    for i in range(4):
-        if numbers[i] + 1 == numbers[i + 1]:
-            return False
-    return True
+def is_valid_combination(numbers: List[int]) -> bool:
+    even_count = sum(num % 2 == 0 for num in numbers)
+    low_count = sum(num <= 25 for num in numbers)
+    return 2 <= even_count <= 3 and 2 <= low_count <= 3 and all(numbers[i] + 1 != numbers[i + 1] for i in range(4))
 
 # Function to check if the combination is a popular sequence
-def is_popular_sequence(numbers):
+def is_popular_sequence(numbers: List[int]) -> bool:
     return all(numbers[i] + 1 == numbers[i + 1] for i in range(4))
 
 # Function to generate ticket without avoiding previous winning combinations
-def generate_ticket_without_avoiding_winners(main_freq, lucky_star_freq):
+def generate_ticket_without_avoiding_winners(main_freq: Counter, lucky_star_freq: Counter) -> Tuple[List[int], List[int]]:
     main_numbers_pool = [num for num, freq in main_freq.items() for _ in range(freq)]
     lucky_star_pool = [num for num, freq in lucky_star_freq.items() for _ in range(freq)]
     main_numbers = []
@@ -30,11 +24,11 @@ def generate_ticket_without_avoiding_winners(main_freq, lucky_star_freq):
     return main_numbers, lucky_stars
 
 # Load the historical EuroMillions draw data
-file_path = "C:\Users\userV\Downloads\euromillions-draw-history.csv" # Replace with the path to your CSV file
+file_path = "C:\\Users\\userV\\Downloads\\euromillions-draw-history.csv" # Replace with the path to your CSV file
 data = pd.read_csv(file_path)
 
 # Analyze the frequency of main numbers and lucky stars
-main_numbers_frequency = Counter(data[f'Ball {i+1}'].values.flatten() for i in range(5))
+main_numbers_frequency = Counter(number for i in range(5) for number in data[f'Ball {i+1}'])
 lucky_star_frequency = Counter(data['Lucky Star 1'].append(data['Lucky Star 2']))
 
 # Generate the lucky dip ticket without avoiding previous winning combinations
